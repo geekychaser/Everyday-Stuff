21b183523bec96de74d120321e978eef9ae2f05c
RishabhKr bothra <rbothra77@gmail.com>
2018-07-19 14:09:57 +0530

    some edits

--- c++/Binary-search-tree/deletionAVL.cpp
+++ c++/Binary-search-tree/deletionAVL.cpp
@@ -1,209 +1,166 @@
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
 
-struct node
-{
-	int data;
-	node * left = nullptr;
-	node * right = nullptr;
-	int height;
+struct node{
+	int data, height;
+	node *left = nullptr, *right = nullptr;
 };
 
-struct node * newNode(int key)
-{
-	node * temp = new node;
-	temp->data = key;
-	temp->left = nullptr;
-	temp->right = nullptr;
+node* newNode(int data){
+	node *temp = new node;
+	temp->data = data;
 	temp->height = 1;
 
 	return temp;
 }
-int height(node * root)
-{
+
+int height(node *root){
+
 	if(root == nullptr)
-	{
 		return 0;
-	}
+
 	return root->height;
 }
-node * leftRotate(node * x)
-{
-	node * y = x->right;
-	node * t = y->left;
 
-	y->left = x;
-	x->right = t;
+int get_balance_factor(node *root){
 
-	x->height = max(height(x->left),height(x->right)) + 1;
-	y->height = max(height(y->left),height(y->right)) + 1;
+	if(root == nullptr)
+		return 0;
+
+	return height(root->left) - height(root->right);
+}
+
+node* right_rotate(node *z){
+
+	node *y = z->left;
+	node *t2 = y->right;
+
+	y->right = z;
+	z->left = t2;
+
+	z->height = max((height(z->left)), (height(z->right))) + 1;
+	y->height = max((height(y->left)), (height(y->right))) + 1;
 
 	return y;
 }
-node * rightRotate(node * y)
-{
-	node * x = y->left;
-	node * t = x->right;
 
-	x->right = y;
-	y->left = t;
+node* left_rotate(node *z){
 
-	y->height = max(height(x->left),height(x->right)) + 1;
-	x->height = max(height(y->left),height(y->right)) + 1;
+	node *y = z->right;
+	node *t2 = y->left;
 
-	return x;
+	y->left = z;
+	z->right = t2;
 
+	z->height = max((height(z->left)), (height(z->right))) + 1;
+	y->height = max((height(y->left)), (height(y->right))) + 1;
+
+	return y;
 }
 
-int getBalance(node * root)
-{
+node* insert(node *root, int key){
+
 	if(root == nullptr)
-	{
-		return 0;
-	}
-	return height(root->left) - height(root->right);
-}
-node * insert(node * root,int key)
-{
-	if(root == nullptr)
-	{
 		return newNode(key);
-	}
 
 	if(key < root->data)
-	{
-		root->left = insert(root->left,key);
-	}
-	else if(key > root->data)
-	{
-		root->right = insert(root->right,key);
-	}
+		root->left = insert(root->left, key);
+
+	else if(root->data < key)
+		root->right = insert(root->right, key);
 
 	else return root;
 
-	root->height = max(height(root->left),height(root->right)) + 1;
+	root->height = max(height(root->left), height(root->right)) + 1;
 
-	int balance = getBalance(root);
+	int balance_factor = get_balance_factor(root);
 
-	if(balance < -1)
-	{
-		if(key > root->right->data)
-		{
-			return leftRotate(root);
-		}
-		if(key < root->right->data)
-		{
-			root->right = rightRotate(root->right);
-			return leftRotate(root);
-		}
+	if(balance_factor > 1 && key < root->left->data)
+		return right_rotate(root);
+
+	if(balance_factor > 1 && key > root->left->data){
+		root->left = left_rotate(root->left);
+		return right_rotate(root);
 	}
-	if(balance > 1)
-	{
-		if(key > root->left->data)
-		{
-			root->left = leftRotate(root->left);
-			return rightRotate(root);
-		}
-		if(key < root->left->data)
-		{
-			return rightRotate(root);
-		}
+
+	if(balance_factor < -1 && key > root->right->data)
+		return left_rotate(root);
+
+	if(balance_factor < -1 && key < root->right->data){
+		root->right = right_rotate(root->right);
+		return left_rotate(root);
 	}
 
 	return root;
 }
-node * minValueNode(node* node)
-{
-    struct node* current = node;
- 
-    while (current->left != NULL)
-        current = current->left;
- 
-    return current;
+
+node* min_val_node(node *root){
+
+	node *temp = root;
+
+	while(temp->left != nullptr)
+		temp = temp->left;
+
+	return temp;
 }
 
-node * deleteNode(node * root,int key)
-{
+node* delete_node(node *root, int key){
+
 	if(root == nullptr)
-	{
 		return root;
-	}
 
 	if(key < root->data)
-	{
-		root->left = deleteNode(root->left,key);
-	}
+		root->left = delete_node(root->left, key);
 
-	else if(key > root->data)
-	{
-		root->right = deleteNode(root->right,key);
-	}
-	
-	else
-	{
-		if(root->left == nullptr)
-		{
-			node * temp = root->right;
+	else if(root->data < key)
+		root->right = delete_node(root->right, key);
+
+	else{
+
+		if(root->left == nullptr){
+			node *temp = root->right;
 			delete root;
-
-			return temp;
-		}
-		else if(root->right == nullptr)
-		{
-			node * temp = root->left;
-			delete root;
-
 			return temp;
 		}
 
-		node * temp = minValueNode(root->right);
+		if(root->right == nullptr){
+			node *temp = root->left;
+			delete root;
+			return temp;
+		}
 
+		node *temp = min_val_node(root->right);
 		root->data = temp->data;
 
-
-		root->right = deleteNode(root->right,temp->data);
-
+		root->right = delete_node(root->right, temp->data);
 	}
 
+	if(root == nullptr)
+		return root;
 
- 
-	root->height = max(height(root->left),height(root->right)) + 1;
+	root->height = max(height(root->left), height(root->right)) + 1;
 
-	int balance = getBalance(root);
+	int balance_factor = get_balance_factor(root);
 
-	if(balance > 1)
-	{
-		if(getBalance(root->left) >= 0)
-		{
-			return rightRotate(root);
-		}
-		else if(getBalance(root->left) < 0)
-		{
-			root->left = leftRotate(root->left);
+	if(balance_factor > 1 && get_balance_factor(root->left) >= 0)
+		return right_rotate(root);
 
-			return rightRotate(root);
-		}
+	if(balance_factor > 1 && get_balance_factor(root->left) < 0){
+		root->left = left_rotate(root->left);
+		return right_rotate(root);
 	}
-	else if(balance < -1)
-	{
-		if(getBalance(root->right) <= 0)
-		{
-			return leftRotate(root);  
-		}
-		else if(getBalance(root->right) > 0)
-		{
-			root->right = rightRotate(root->right);
 
-			return leftRotate(root);
-		}
+	if(balance_factor < -1 && get_balance_factor(root->right) <= 0)
+		return left_rotate(root);
+
+	if(balance_factor < -1 && get_balance_factor(root->right) > 0){
+		root->right = right_rotate(root->right);
+		return left_rotate(root);
 	}
+
 	return root;
-
-
 }
-
 
 void inorder(node * root)
 {
@@ -220,30 +177,32 @@ void inorder(node * root)
 }
 
 
-int main()
-{
-	node *root = NULL;
-	root = insert(root, 10);
-	root = insert(root, 20);
-	root = insert(root, 30);
-	root = insert(root, 40);
-	root = insert(root, 50);
-	root = insert(root, 25);
+int main(){
 
-	inorder(root);
-
-	cout<<endl;
-
-	deleteNode(root,30);
-	deleteNode(root,25);
-
-	inorder(root);
-
-	cout<<endl;
+	struct node *root = nullptr;
  
 
+    root = insert(root, 9);
+    root = insert(root, 5);
+    root = insert(root, 10);
+    root = insert(root, 0);
+    root = insert(root, 6);
+    root = insert(root, 11);
+    root = insert(root, -1);
+    root = insert(root, 1);
+    root = insert(root, 2);
+ 
+    
 
-    return 0;
+	inorder(root);
+	cout<<endl;
+
+	root = delete_node(root, 10);
 
 
+	
+
+	inorder(root);
+	cout<<endl;
+	return 0;
 }
