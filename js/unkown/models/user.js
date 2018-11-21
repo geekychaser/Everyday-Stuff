fa1de3a0c07dac643f3c4f55c0c027e8565a1810
RishabhKr bothra <rbothra77@gmail.com>
2018-11-21 14:19:33 +0530

    finally connected to local db

--- js/unkown/models/user.js
+++ js/unkown/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -6,6 +7,20 @@ var userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
+    },
+    age:{
+        type: Number,
+        required: true,
+    },
+    adhaar: {
+        type: Number,
+        minlength:12,
+        maxlength:12,
+        required: true,
+    },
+    marital:{
+        type: String,
+        required: true,
     },
     email: {
         type: String,
@@ -18,5 +33,24 @@ var userSchema = new Schema({
         required: true,
     }
 });
+
+
+// userSchema.pre('save',(next)=>{
+//     console.log(this);
+//     var user = userSchema;
+//     console.log(user);
+//     // this.password = bcrypt.hashSync(this.password,8);
+//     bcrypt.genSalt(10, (err,salt)=>{
+//         bcrypt.hash(user.password, salt, (err,hash)=>{
+//             if(err) console.log("this is fucking error" + err);
+//             console.log("this is fucking password " + user.password);
+//             console.log("this is fucking hash "+ hash);
+//             user.password = hash;
+
+//             console.log("\n this is fucking new password " + user.password);
+//             next();
+//         });
+// });
+// })
 
 module.exports = mongoose.model("User",userSchema);
--- js/unkown/server.js
+++ js/unkown/server.js
@@ -3,28 +3,39 @@ var bodyParser = require('body-parser');
 var mongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var session = require('express-session');
+var expressValidator = require('express-validator');
 var app = express();
+var User = require('./models/user');
+var bcrypt = require('bcryptjs');
+const port = process.env.PORT || 8000;
 
 app.set('view engine','ejs');
 //app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-    secret: "budhi bhai",
-    resave: true,
-    saveUninitialized: false
-}));
+app.use(expressValidator());
 
-const uri = "mongodb+srv://Budhiraja:9811809871@cluster0-apyn6.mongodb.net/test?retryWrites=true"
+var dburl = "mongodb://localhost:27017/budhi";
+mongoose.Promise = global.Promise;
+mongoose.connect(dburl);
 
-mongoClient.connect(uri,{ useNewUrlParser: true },(err,client)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log("connected..");
-    const collection = client.db("test").collection("devices");
-    client.close();
-})
+// app.use(session({
+//     secret: "budhi bhai",
+//     resave: true,
+//     saveUninitialized: false
+// }));
+
+// const uri = "mongodb+srv://Budhiraja:9811809871@cluster0-apyn6.mongodb.net/test?retryWrites=true"
+
+// mongoClient.connect(uri,{ useNewUrlParser: true },(err,client)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("connected..");
+//     const collection = client.db("test").collection("devices");
+//     client.close();
+// })
 
 app.get('/',(req,res)=>{
     //res.send("started again");
@@ -36,8 +47,30 @@ app.get('/signup',(req,res)=>{
 })
 
 app.post('/signup',(req,res)=>{
-    console.log(req.body);
-    res.redirect('/');
+    // console.log(req.body);
+    // console.log(req.body.email);
+    // console.log(req.body.password);
+    // req.checkBody('name', 'Name is required').notEmpty();
+    // req.checkBody('email', 'Email is required').notEmpty();
+    // req.checkBody('email', 'Please enter a valid email').isEmail();
+    // req.checkBody('password','Please enter password').notEmpty();
+
+
+    var myData = new User(req.body);
+    console.log("this is fucking password " + myData.password);
+    myData.password =  bcrypt.hashSync(myData.password,8);
+    console.log("this is fucking hash " + myData.password);
+    myData.save().then(()=>{
+        res.redirect('/');
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send("operation failed try again");
+    });
+
+    
+    
+    
 })
 
 app.get('/login',(req,res)=>{
@@ -49,8 +82,8 @@ app.post('/login',(req,res)=>{
     res.redirect('/');
 })
 
-app.listen(8000,()=>{
-    console.log("server started");
+app.listen(port,()=>{
+    console.log("server started and listening to port " + port);
 })
 
 //mongodb+srv://Budhiraja:<PASSWORD>@cluster0-apyn6.mongodb.net/test?retryWrites=true
