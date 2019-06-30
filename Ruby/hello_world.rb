c00999374ec698136d09a70b18cd60ecd48f420c
RishabhKr bothra <rbothra77@gmail.com>
2019-06-30 13:51:14 +0530

    removed ruby

--- Ruby/hello_world.rb
+++
@@ -1,8 +0,0 @@
-class Sample
-    def hello
-        puts "hello world!"
-    end
-end
-
-s = Sample.new
-s.hello
--- Ruby/toy_app/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-    has_many :microposts
-    validates :email, length: { maximum: 140}, presence: true
-    validates :name, length: { maximum: 140}, presence: true
-end
