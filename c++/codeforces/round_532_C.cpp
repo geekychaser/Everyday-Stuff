b9cda15b3ebd4169fe1f2f5cf103049b3726b3b9
RishabhKr bothra <rbothra77@gmail.com>
2020-03-18 08:02:41 +0530

    round 532 C

---
+++ c++/codeforces/round_532_C.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define PI 3.14
+
+using namespace std;
+
+int main(){
+    int n,r;
+
+    cin>>n>>r;
+
+    double num = r*sin(PI/n);
+    double den = 1 - sin(PI/n);
+
+    cout<<fixed<<setprecision(7)<<num/den;
+}
