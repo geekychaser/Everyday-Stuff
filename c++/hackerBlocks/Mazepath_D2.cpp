19795ffb3504b1f9c0ce8ad95feb935b5827b071
RishabhKr bothra <rbothra77@gmail.com>
2020-04-22 03:14:10 +0530

    maze path D with little contraints on diagonal

---
+++ c++/hackerBlocks/Mazepath_D2.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MAX 100
+
+void start(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+		freopen("in.txt","r",stdin);
+		freopen("out.txt","w",stdout);
+	#endif
+
+}
+
+int counter = 0;
+
+void solve(int n,int m,int i,int j,string ans){
+
+	if(i == n-1 && j == m-1){
+		cout<<ans<<" ";
+		counter++;
+		return;
+	}
+
+	if(i+1 < n){
+		// ans += "(";
+		solve(n,m,i+1,j,ans+'V');
+	}
+
+	if(j+1 < n){
+		 solve(n,m,i,j+1,ans+'H');
+	}
+
+	if(i+1 < n && j+1 < m && (i == j || i+j == n-1)){
+		solve(n,m,i+1,j+1,ans+'D');
+	}
+	
+}
+
+
+
+int32_t main(){
+	fastIO
+	// start();
+	string ans = "";
+
+	int n,m;
+	cin>>n;
+
+	solve(n,n,0,0,ans);
+	cout<<endl<<counter<<endl;
+
+
+}
