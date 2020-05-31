5f07da9c2108851b7e8a1c3c6c249c63b204c415
RishabhKr bothra <rbothra77@gmail.com>
2020-05-31 22:14:53 +0530

    print-adjacency-list gfg

---
+++ c++/hackerBlocks/graph/imp.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Graph{
+	unordered_map<int,vector<int> >m;
+
+public:
+
+	Graph(){
+
+	}
+
+	void add_edge(int x,int y, bool bidir = true){
+		m[x].push_back(y);
+		if(bidir){
+			m[y].push_back(x);
+		}
+	}
+
+	void print(int v){
+
+		for(int it=0;it<v;it++){
+			cout<<it;
+			for(auto i: m[it]){
+				cout<<"->"<<i;
+			}
+			cout<<endl;
+		}
+	}
+
+
+};
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		int v,e;
+		cin>>v>>e;
+
+		Graph g;
+		while(e--){
+			int x,y;
+			cin>>x>>y;
+
+			g.add_edge(x,y);
+
+		}
+
+		g.print(v);
+	}
+}
