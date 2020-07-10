263ba8abfce981587ff3ca448ea22ccfa286412f
RishabhKr bothra <rbothra77@gmail.com>
2020-07-10 17:21:28 +0530

    filter reduce, find and findIndex finctions

--- js/WEBdev/js_playground/examples.js
+++ js/WEBdev/js_playground/examples.js
@@ -28,3 +28,29 @@ let result = arr.reduce((acc,item) =>{
 },0)
 
 console.log(result);
+
+
+//filter function is used to filter out some element for original array according to our need as its name suggests
+// it only accepts true and false , adds the elemt if recieves true and leave the element is recieves false
+
+let filter_arr = arr.filter((item) => {
+    return item%2 == 0;
+});
+
+console.log(filter_arr);
+
+
+// Array function find and findIndex both are used to find no. and its index based on some conditions (similar as linear search based on some conditon)
+
+// find first even number in the arr list
+let find_num = arr.find((item) => {
+    return item%2 == 0;
+});
+
+// find index of first even number in arr list
+let find_idx = arr.findIndex((item) =>{
+    return item%2 == 0;
+})
+
+console.log(find_num);
+console.log(find_idx);
