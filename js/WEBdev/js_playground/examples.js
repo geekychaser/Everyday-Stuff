96c0cea9d19e16a65a318115b6da3ed2a9e14709
RishabhKr bothra <rbothra77@gmail.com>
2020-07-10 16:58:35 +0530

    some basics of ES6

---
+++ js/WEBdev/js_playground/examples.js
@@ -0,0 +1,10 @@
+
+// using map for acessing the arr element dircetly i.e shortcut for for loop
+
+let arr = [1,2,3,4,5,6];
+
+let new_arr = arr.map((item) => {
+    return item*2;
+})
+
+console.log(new_arr);
