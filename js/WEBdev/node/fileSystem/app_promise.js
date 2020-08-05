e57d1611c3d96060b5eb2d6ffc6e800bf56f4bd2
RishabhKr bothra <rbothra77@gmail.com>
2020-08-05 17:22:22 +0530

    file system using promises

---
+++ js/WEBdev/node/fileSystem/app_promise.js
@@ -0,0 +1,55 @@
+// taking input from 2 inut files and putting them in third file after sorting them using promises to avoid callback hell
+
+const fs = require('fs');
+const path = require('path');
+const F1 = path.join(__dirname,'inp1.txt');
+const F2 = path.join(__dirname,'inp2.txt');
+const F3 = path.join(__dirname,'output.txt');
+
+function read1(){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(F1,(err,data)=>{
+            if(err) throw err;
+            data = data.toString().split('\n');
+            resolve(data);
+        })
+    })
+}
+
+function read2(){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(F2,(err,data)=>{
+            if(err) throw err;
+            data = data.toString().split('\n');
+            resolve(data);
+        })
+    })
+}
+
+function write(ans){
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(F3,ans,(err)=>{
+            if(err) throw err;
+            resolve();
+        })
+    })
+}
+
+let ans;
+
+read1()
+    .then((data)=>{
+        ans = data;
+        console.log("data read from file 1 successfull !");
+        return read2();
+    })
+    .then((data)=>{
+        ans = ans.concat(data);
+        console.log("read from file 2 successfull !");
+        ans.sort((a,b)=>a-b);
+        ans = ans.join('\n');
+        return write(ans);
+    })
+    .then(()=>{
+        console.log("write is successfull !");
+    })
