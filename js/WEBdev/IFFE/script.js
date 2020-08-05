53b18fea492df29fb4e3e73486f00b3ea96a3fb1
RishabhKr bothra <rbothra77@gmail.com>
2020-08-05 18:12:54 +0530

    immediate invoke function expression

---
+++ js/WEBdev/IFFE/script.js
@@ -0,0 +1,7 @@
++function(){
+    console.log('this is iffe function');
+}()
+
+// immediate invoke function expression these are used to avoid ploouting global namespace as var inside iffe are not accessible outside the function
+
+//uglifu js used reduce file size javascript
