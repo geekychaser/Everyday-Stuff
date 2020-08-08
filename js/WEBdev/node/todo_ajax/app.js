c824fc672ac59b838a707bc39c1991e183ae1ef2
RishabhKr bothra <rbothra77@gmail.com>
2020-08-08 18:06:49 +0530

    fully completed with backend only styling left

--- js/WEBdev/node/todo_ajax/app.js
+++ js/WEBdev/node/todo_ajax/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-let todos = new Map();
-let task_counter = 0;
+let todos = [];
+// let task_counter = 0;
 
 app.use('/',express.static(path.join(__dirname,'static')));
+// app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // route to add todos
 app.get('/addTodo',(req,res)=>{
@@ -17,11 +20,11 @@ app.get('/addTodo',(req,res)=>{
         res.send("Task can not be empty !");
     
     //check if the task is already present
-    else if(todos.has(task) == true) 
+    else if(todos.includes(task) == true) 
         res.send("task is alread present !");
     else{
-        todos.set(task,task_counter);
-        task_counter++;
+        todos.push(task);
+        // task_counter++;
         res.send("success");
     }
 })
@@ -31,6 +34,12 @@ app.get('/todo',(req,res)=>{
 
     //Spread operator to pass map and make it as obj string so that we can send it as a response as map can not be send
     res.send( JSON.stringify([...todos]));
+})
+
+// route to update the todos value
+app.post('/update',(req,res)=>{
+    res.send("success update");
+    todos = req.body.data;
 })
 
 app.listen(3000,()=>{
--- js/WEBdev/node/todo_ajax/static/script.js
+++ js/WEBdev/node/todo_ajax/static/script.js
@@ -1,18 +1,66 @@
 
 $(document).ready(()=>{
 
+    let data;
+
+    function Swap(list, iA, iB){
+        [list[iA], list[iB]] = [list[iB], list[iA]];
+        return list;
+      }
     function refreshTodos(){
         $('#list').empty();
 
         $.get('/todo',(info)=>{
             
             // Converting the string object back into map
-            let data = new Map(JSON.parse(info));
+            data = JSON.parse(info);
             
-            for(let [key,value] of data){
-                console.log(key);
+            for(let key of data){
+                // console.log(key);
                 $('#list').append(
-                    $('<li>').text(key)
+                    $('<li>').text(key).append([
+                        $('<i>',{"class": "arrow up"}).click((ev)=>{
+                            $(ev.target).parent().insertBefore($(ev.target).parent().prev())
+
+                            //Finding the index of elment which is to be swapped
+                            let iA = data.indexOf($(ev.target).parent().text());
+                            data = Swap(data,iA,iA-1);
+
+                            //Post request to update the data array on server
+                            $.post('/update',{data :data},(message)=>{
+                                console.log(message);
+                            })
+
+                        }),
+                        $('<i>',{"class": "arrow down"}).click((ev)=>{
+                            $(ev.target).parent().insertAfter($(ev.target).parent().next())
+
+                            //Finding the index of elment which is to be swapped
+                            let iA = data.indexOf($(ev.target).parent().text());
+                            data = Swap(data,iA,iA+1);
+
+                            //Post request to update the data array on server
+                            $.post('/update',{data :data},(message)=>{
+                                console.log(message);
+                            })
+                        }),
+
+                        $('<i>',{"class": "fas fa-trash"}).click((ev)=>{
+                            let iA = data.indexOf($(ev.target).parent().text());
+
+                            // Removing that element from list
+                            $(ev.target).parent().remove();
+
+                            // Romving that element from data array
+                            data.splice(iA,1);
+
+                            //Post request to update the data array on server
+                            $.post('/update',{data :data},(message)=>{
+                                console.log(message);
+                            })
+
+                        })
+                    ])
                 )
             }
         });
@@ -24,9 +72,8 @@ $(document).ready(()=>{
     $('#btn').click(()=>{
 
         let task = $('#inp').val();
-        console.log(task);
+        // console.log(task);
             $.get(`/addTodo?q=${task}`,(message)=>{
-                console.log('req toh gyi')
                 refreshTodos();
                 alert(message);
             })
