d1b7cc3591082cddd43f143cb67f39a4b8542686
RishabhKr bothra <rbothra77@gmail.com>
2020-08-10 20:37:32 +0530

    todo only with handlebars completed

--- js/WEBdev/node/non_js_todo/app.js
+++ js/WEBdev/node/non_js_todo/app.js
@@ -1,24 +1,49 @@
 const express = require('express');
+const path = require('path');
+const { handlebars } = require('hbs');
 const app = express();
 
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static('public'));
+app.set('view engine','hbs');
 
-let todos = [];
+// Array to save todos
+let todos = ['hello','world'];
+
+//Handlebar helper function to print list
+handlebars.registerHelper("list",function(options){
+    var res = '<ul>';
+
+    for(let i=0;i<todos.length;i++){
+        res = res + `<form action="/del" method="POST"><input name="name" value="${todos[i]}" style="display: none;">`
+        res = res + `<li> ${todos[i]}<button type="submit">del</button></li></form>`;
+    }
+
+    return res + '</ul>';
+})
+
+
 
 app.get('/',(req,res)=>{
-    res.send(todos);
+    // res.send(todos);
+    res.render('index');
 })
 
+// post request to add task in todos
 app.post('/',(req,res)=>{
-    // console.log(req.body);
+
     todos.push(req.body.task);
-    res.send(todos);
+    res.redirect('/');
 })
 
-app.put('/',(req,res)=>{
-    // logic for swapping the elements
+app.post('/del',(req,res)=>{
+    // delet the task requested
+    console.log(req.body);
+    const idx = todos.indexOf(req.body.name);
+    todos.splice(idx,1);
+    res.redirect('/');
 })
 
 app.listen(3000,()=>{
