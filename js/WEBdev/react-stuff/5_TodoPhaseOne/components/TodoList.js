0d5bbcdc4507fc632f9995a1e7b9733851f74ceb
geekychasser <rbothra77@gmail.com>
2020-08-28 14:22:18 +0530

    tsrting with todos

---
+++ js/WEBdev/react-stuff/5_TodoPhaseOne/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TodoList(){
+    return(
+        <div>
+            <input type="checkbox"></input>
+            <label>Complete react tutorial</label>
+            <br></br>
+            <input type="checkbox"></input>
+            <label>Ready about cookies and JWT</label>
+            <br></br>
+            <input type="checkbox"></input>
+            <label>Build some project or revise ds algo</label>
+        </div>
+    )
+}
+export default TodoList;
