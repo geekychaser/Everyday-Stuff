e169d0809a9bbfdb7f3a494828c9496d7b52f386
geekychasser <rbothra77@gmail.com>
2020-08-28 13:40:58 +0530

    functional component and their stucture

---
+++ js/WEBdev/react-stuff/3_SeperateFunctionComponent/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from  "react";
+
+function MyInfo(){
+    return (
+        <div>
+            <h1>Rishabh Kumar Bothra</h1>
+            <p>Stuff i really need to do !</p>
+            <ul>
+                <li>ask budhiraja for topic</li>
+                <li>Complete react tutorial asap</li>
+                <li>build few projects</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyInfo;
---
+++ js/WEBdev/react-stuff/3_SeperateFunctionComponent/index.js
@@ -0,0 +1,4 @@
+import MyInfo from "./components/MyInfo";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<MyInfo />,document.getElementById("root"));
