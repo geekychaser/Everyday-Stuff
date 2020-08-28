9585ae5ba5169eaabbb48b67dbf18320a73f82c6
geekychasser <rbothra77@gmail.com>
2020-08-28 14:00:08 +0530

    dividing in parent and child component

---
+++ js/WEBdev/react-stuff/4_ParentChlid/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div>
+            <span>LOGO</span>
+            <ul>
+                <li>NAV_1</li>
+                <li>NAV_2</li>
+                <li>NAV_3</li>
+            </ul>
+        </div>
+    )
+}
+export default Header;
---
+++ js/WEBdev/react-stuff/4_ParentChlid/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Main(){
+    return (
+        <p>Here goes my body that is my main content</p>
+    )
+}
+
+
+export default Main;
