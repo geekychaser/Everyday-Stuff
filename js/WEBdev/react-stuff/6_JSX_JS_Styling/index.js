bb74c34e556aed2edae3bf2a16c3bdb02c8b239f
geekychasser <rbothra77@gmail.com>
2020-08-28 14:56:41 +0530

    different and dynamic ways of styling

--- js/WEBdev/react-stuff/6_JSX_JS_Styling/index.js
+++ js/WEBdev/react-stuff/6_JSX_JS_Styling/index.js
@@ -7,20 +7,42 @@ const App = ()=>{
     const date = new Date();
     const hours = date.getHours();
     let time;
+    let goodStyle = {
+        
+    }
 
     if(hours < 12){
         time = "morning";
+        goodStyle.color = "blue";
     }
     else if(hours >= 12 && hours < 17){
-        time = "afternoon"
+        time = "afternoon";
+        goodStyle.color = "orange";
     }
     else{
-        time = "night"
+        time = "night";
+        goodStyle.color = "grey"
+    }
+
+    let spanStyles = {
+        backgroundColor:"black",
+        color:"white",
     }
 
     return(
-        // we need to use className instead of class in JSX
-    <h2 className="test">Good {time} !</h2>
+      <div>
+      {/* we need to use className instead of class in JSX */}
+      <h2 className="test">Good {time} !</h2>
+
+      {/* iniline style are applied using objects and style with "-" are replaced by camelCase */}
+      <p style={{color:"red",backgroundColor:"lightblue"}}>Just to test some inline styling</p>
+
+      {/*Simply attaching style object as complete looks odd */}
+      <span style={spanStyles}>This is another test text</span>
+
+      {/* Now making good moring n all dyanmic color change*/}
+    <h3 style={goodStyle}>Good {time} !</h3>
+    </div>
     )
 }
 
